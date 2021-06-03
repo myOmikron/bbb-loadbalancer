fee50071a13f8c3caa7f2a63a47b76e760eeedb7
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-06-03 19:09:07 +0200

    Added server to meetings table

--- bbb_loadbalancer/api/admin.py
+++ bbb_loadbalancer/api/admin.py
@@ -18,4 +18,4 @@ class BBBServerAdmin(admin.ModelAdmin):
 
 @admin.register(Meeting)
 class MeetingAdmin(admin.ModelAdmin):
-    list_display = ("__str__", "ended")
+    list_display = ("ended", "__str__", "server")
