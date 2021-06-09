b72f4fbd7ff57b5ea017e1ff1c3d272a7c367bf6
myOmikron <accounts@omikron.dev>
2021-06-09 15:57:45 +0200

    Added app for poller

--- bbb_loadbalancer/common_files/apps.py
+++ bbb_loadbalancer/common_files/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class CommonFilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'common_files'
+
+
+class CommonFilesConfigPoller(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bbb_loadbalancer.common_files'
