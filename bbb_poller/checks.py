29db82d5007c1c9a30bbcf5abaa198b2e516f918
myOmikron <accounts@omikron.dev>
2021-12-21 02:22:04 +0100

    Added exception handling for timeout

--- bbb_poller/checks.py
+++ bbb_poller/checks.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import os
+import socket
 
 from bigbluebutton_api_python import BigBlueButton
 from bigbluebutton_api_python.exception import BBBException
@@ -68,7 +69,7 @@ def get_running_meetings(meeting_id, server):
         b = BigBlueButton(server.url, server.secret)
         try:
             b.get_meeting_info(meeting_id)
-        except BBBException:
+        except BBBException or socket.timeout:
             await asyncio.sleep(0.01)
             return False
         await asyncio.sleep(0.01)
